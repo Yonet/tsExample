b4d6331d23be57531a8d05fe60e3f57c5dc234f2
Aysegul Yonet <aysegulyonet@gmail.com>
2015-07-26 19:42:09 -0700

    Initial commit.

---
+++ app.js
@@ -0,0 +1,2 @@
+//imports the type definitions from Angular
+/// <reference path="typings/angular2/angular2.d.ts" />
---
+++ app.ts
@@ -0,0 +1,5 @@
+//imports the type definitions from Angular
+/// <reference path="typings/angular2/angular2.d.ts" />
+
+//ES6 module syntax to import three symbols from the Angular module. The module will load at runtime.
+import {Component, View, bootstrap} from 'angular2/angular2';
