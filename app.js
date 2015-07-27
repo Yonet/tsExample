66c9bf894b3a867d6c84f84521452cc3325561cc
Aysegul Yonet <aysegulyonet@gmail.com>
2015-07-26 20:19:57 -0700

    Hello world example with ts.

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
 //imports the type definitions from Angular
 /// <reference path="typings/angular2/angular2.d.ts" />
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
+    switch (arguments.length) {
+        case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
+        case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
+        case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
+    }
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+//ES6 module syntax to import three symbols from the Angular module. The module will load at runtime.
+var angular2_1 = require('angular2/angular2');
+var MyAppComponent = (function () {
+    function MyAppComponent() {
+        this.name = 'Aysegul';
+    }
+    MyAppComponent = __decorate([
+        angular2_1.Component({
+            selector: 'my-app' // Defines the <my-app></my-app> tag
+        }),
+        angular2_1.View({
+            template: '<h1>Hello {{ name }}</h1>' // Defines the inline template for the component
+        }), 
+        __metadata('design:paramtypes', [])
+    ], MyAppComponent);
+    return MyAppComponent;
+})();
+angular2_1.bootstrap(MyAppComponent);
--- app.ts
+++ app.ts
@@ -3,3 +3,19 @@
 
 //ES6 module syntax to import three symbols from the Angular module. The module will load at runtime.
 import {Component, View, bootstrap} from 'angular2/angular2';
+
+@Component({
+	selector: 'my-app' // Defines the <my-app></my-app> tag
+})
+@View({
+	template: '<h1>Hello {{ name }}</h1>' // Defines the inline template for the component
+})
+
+class MyAppComponent {
+	name: string;
+	constructor() {
+		this.name = 'Aysegul';
+	}
+}
+
+bootstrap(MyAppComponent);
